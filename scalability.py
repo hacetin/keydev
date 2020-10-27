22508a8843573dec6b3ac15d596bf9c847e3fc4b
Alperen Çetin <hamdialperencetin@gmail.com>
2020-10-27 16:21:58 +0300

    updated scalability for all rqs

--- scalability.py
+++ scalability.py
@@ -1,12 +1,14 @@
-from statistics import leaving_developers_table
+"""
+Runs algorithms and prints time statistics for scalability discussions.
+"""
 from time import perf_counter
 from graph import HistoryGraph
 from joblib import Parallel, delayed
-from main import find_leaving_developers
+from util import find_leaving_developers
 
 
 @delayed
-def rq1_experiment(project_name):
+def scalability_experiment_rq1_rq3(project_name, method_name):
     """
     Run experiment with default parameters. First, create a graph for the inital
     window, then slide the window day by day. Find developers, mavens, connectors
@@ -16,6 +18,9 @@ def rq1_experiment(project_name):
     ----------
     project_name (str):
         Name of the project.
+    method name (str):
+        Name of the method to run in the experiment. A method with the same name have to
+        be defined in graph.HistoryGraph. For example, "get_connectors".
 
     Returns
     -------
@@ -49,9 +54,7 @@ def rq1_experiment(project_name):
         total_num_edges += G.get_num_edges()
 
         t_start = perf_counter()
-        G.get_jacks()
-        G.get_mavens()
-        G.get_connectors()
+        r = eval("G.{}()".format(method_name))
         t_end = perf_counter()
         time_taken += t_end - t_start
 
@@ -80,7 +83,7 @@ def rq1_experiment(project_name):
 
 
 @delayed
-def rq2_experiment(project_name):
+def scalability_experiment_rq2(project_name):
     """
     Run experiment with default parameters. First, create a graph for the inital
     window, then slide the window day by day. Find replacements for leaving
@@ -105,7 +108,6 @@ def rq2_experiment(project_name):
     G = HistoryGraph(dataset_path)
     date_to_leaving_developers = find_leaving_developers(G)
 
-    G = HistoryGraph(dataset_path)
     # Start iterations
     num_leaving_developers = 0
     total_num_nodes = 0
@@ -153,43 +155,48 @@ def rq2_experiment(project_name):
     )
 
 
-if __name__ == "__main__":
+def run_experiment_rq1_rq3(method_name):
     project_names = ["hadoop", "hive", "pig", "hbase", "derby", "zookeeper"]
+    print("Running experiments for {}. It might take a while.".format(method_name))
 
-    print("RQ1 - Identifying Key Developers")
-    print("Running experiments can take hours.")
-
-    res_rq1 = Parallel(n_jobs=-1, verbose=10)(
-        rq1_experiment(project_name) for project_name in project_names
+    results = Parallel(n_jobs=-1, verbose=10)(
+        scalability_experiment_rq1_rq3(pname, method_name) for pname in project_names
     )
 
+    print("\n\nMethod: {}\n".format(method_name))
+    print_results(results)
+
+
+def run_experiment_rq2():
+    project_names = ["hadoop", "hive", "pig", "hbase", "derby", "zookeeper"]
+    print("Running experiments for find_replacements. It might take a while.")
+
+    results = Parallel(n_jobs=-1, verbose=10)(
+        scalability_experiment_rq2(pname) for pname in project_names
+    )
+
+    print("\n\nMethod: find_replacements\n")
+    print_results(results)
+
+
+def print_results(res):
     print("Experiment Name\t\t\t| Avg. num. nodes\t| Node Stats ")
-    for exp_name, node_stat, avg_num_nodes, _, _, _, _ in res_rq1:
+    for exp_name, node_stat, avg_num_nodes, _, _, _, _ in res:
         print("{:<32}{:<5}\t\t\t{}".format(exp_name, avg_num_nodes, node_stat))
 
     print("\nExperiment Name\t\t\t| Avg. num. edges\t| Edge Stats ")
-    for exp_name, _, _, edge_stat, avg_num_edges, _, _ in res_rq1:
+    for exp_name, _, _, edge_stat, avg_num_edges, _, _ in res:
         print("{:<32}{:<5}\t\t\t{}".format(exp_name, avg_num_edges, edge_stat))
 
-    print("\nExperiment Name\t\t\t| Avg. time taken\t| Num. iterations")
-    for exp_name, _, _, _, _, avg_time_taken, num_iters in res_rq1:
-        print("{:<32}{:.2f}\t\t\t{}".format(exp_name, avg_time_taken, num_iters))
+    print("\nExperiment Name\t\t\t| Avg. time taken (sec)\t| Num. iterations")
+    for exp_name, _, _, _, _, avg_time_taken, num_iters in res:
+        print("{:<32}{:.4f}\t\t\t{}".format(exp_name, avg_time_taken, num_iters))
 
-    print("RQ2 - Replacement Validation")
-    print("Running experiments can take tens of minutes.")
+    print()
 
-    res_rq2 = Parallel(n_jobs=-1, verbose=10)(
-        rq2_experiment(project_name) for project_name in project_names
-    )
 
-    print("Experiment Name\t\t\t| Avg. num. nodes\t| Node Stats ")
-    for exp_name, node_stat, avg_num_nodes, _, _, _, _ in res_rq2:
-        print("{:<32}{:<5}\t\t\t{}".format(exp_name, avg_num_nodes, node_stat))
+if __name__ == "__main__":
+    run_experiment_rq2()
 
-    print("\nExperiment Name\t\t\t| Avg. num. edges\t| Edge Stats ")
-    for exp_name, _, _, edge_stat, avg_num_edges, _, _ in res_rq2:
-        print("{:<32}{:<5}\t\t\t{}".format(exp_name, avg_num_edges, edge_stat))
-
-    print("\nExperiment Name\t\t\t| Avg. time taken\t| Num. iterations")
-    for exp_name, _, _, _, _, avg_time_taken, num_iters in res_rq2:
-        print("{:<32}{:.2f}\t\t\t{}".format(exp_name, avg_time_taken, num_iters))
+    for mname in ["get_jacks", "get_mavens", "get_connectors", "balanced_or_hero"]:
+        run_experiment_rq1_rq3(mname)
